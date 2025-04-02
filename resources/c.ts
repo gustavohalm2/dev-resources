d1cd1ec9ac836d727d133d824af1a12a398c64f0
gustavohalm2 <150925254+gustavohalm2@users.noreply.github.com>
2025-04-01 22:33:49 -0300

    Add Chargeblast

--- resources/c.ts
+++ resources/c.ts
@@ -86,6 +86,14 @@ export const resources: Resource[] = [
         url: 'https://chakra-ui.com/',
     },
     {
+        name: 'Chargeblast',
+        categories: ['Anlytics', 'Library'],
+        description: 'Chargeblast helps developers simplify their payment stack and not worry about blocking disputes',
+        url: 'https://www.chargeblast.com/',
+        keywords: ['payments', 'chargeback', 'stripe integration'],
+    },
+ 
+    {
         name: 'Chart.js',
         categories: ['Library', 'Programming'],
         description:
